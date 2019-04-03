fec2027e85c23aaf8fa586ad5df0676e980992e9
minhhoang <ngocminhhoang198@gmail.com>
2019-04-03 12:10:21 +0100

    added displayWAVDATA function

--- sound.c
+++ sound.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "sound.h"
+#include <math.h>
 
 void showID(char *idname, char *id){
 	int i;
@@ -24,4 +25,24 @@ void displayWAVHDR(struct WAVHDR h){
 	showID("Subchunk2ID", h.Subchunk2ID);
 	printf("Subchunk2 size: %d\n", h.Subchunk2Size);
 	// to be continue for other fields
+}
+
+// this function gets 1 second of samples (16000), and calculates
+// 80 pieces of decibel value, we know we need to calculate
+// one decibel value from 200 samples, decibel value is calculated
+// by RMS formula
+void displayWAVDATA(short s[]){
+	double rms[80];
+	short *ptr = s;		// we use a pointer, pointing to the begining of array
+	int i, j;		// for nested loop counters, outer loop repeats 80 times
+					// inner loop repeats 200 times
+	for (i = 0; i < 80; i++){
+		double sum = 0;		// accumulate sum of squares
+		for (j = 0; j < 200; j++){
+			sum += (*ptr) * (*ptr);
+			ptr++;		// pointing to the next sample
+		}
+		rms[i] = sqrt(sum/200);
+		printf("rms[%d] = %f\n", i, rms[i]);
+	}
 }
